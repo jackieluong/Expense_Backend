039691acfc9f49f9d58d450de92e2caf0d9cd9f1
ManhTien <manhtien4study@gmail.com>
2025-03-22 09:24:02 +0700

    add config

--- src/main/java/com/BK/Expense/entity/Account.java
+++ src/main/java/com/BK/Expense/entity/Account.java
@@ -41,4 +41,44 @@ public abstract class Account {
         this.password = password;
         this.role = roleEnum;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public RoleEnum getRole() {
+        return role;
+    }
+
+    public void setRole(RoleEnum role) {
+        this.role = role;
+    }
 }
---
+++ src/main/java/com/BK/Expense/security/JwtAuthResponse.java
@@ -0,0 +1,60 @@
+package com.BK.Expense.security;
+
+import com.BK.Expense.enums.RoleEnum;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+// dto class for jwt login response
+public class JwtAuthResponse
+{
+    @JsonProperty("accessToken")
+    private String accessToken;
+
+    @JsonProperty("user")
+    private UserLogin user;
+
+    public JwtAuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public JwtAuthResponse(){};
+
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public UserLogin getUserLogin() {
+        return user;
+    }
+
+    public void setUserLogin(UserLogin userLogin) {
+        this.user = userLogin;
+    }
+
+    @Getter
+    @Setter
+    public static class UserLogin{
+        private long id;
+        private String email;
+        private String name;
+        private RoleEnum role;
+
+        public UserLogin(long id, String email, String name, RoleEnum role) {
+            this.id = id;
+            this.email = email;
+            this.name = name;
+            this.role = role;
+        }
+
+
+    }
+
+
+}
